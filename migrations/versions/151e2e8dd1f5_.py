12ca0ba265580a259e3998212087c97e15160744
Scott Morse <scottgmorse@gmail.com>
2015-05-29 16:48:19 -0700

    Migrate

--- migrations/versions/151e2e8dd1f5_.py
+++ migrations/versions/151e2e8dd1f5_.py
@@ -1,13 +1,13 @@
 """empty message
 
-Revision ID: 3920bc38982f
+Revision ID: 151e2e8dd1f5
 Revises: None
-Create Date: 2015-05-29 15:57:55.898049
+Create Date: 2015-05-29 16:48:00.806258
 
 """
 
 # revision identifiers, used by Alembic.
-revision = '3920bc38982f'
+revision = '151e2e8dd1f5'
 down_revision = None
 
 from alembic import op
