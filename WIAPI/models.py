df119cf24f559bd826c8d3e4324f783b686a3abb
Scott Morse <scottgmorse@gmail.com>
2015-05-29 14:48:57 -0700

    whoops, dont forget this file

---
+++ WIAPI/models.py
@@ -0,0 +1,20 @@
+from WIAPI import db
+from sqlalchemy.dialects.postgresql import JSON
+
+class Offer(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String)
+	description = db.Column(db.String)
+	venue = db.Column(db.String)
+
+	def __init__(self, title, description, venue):
+		self.title = title
+		self.description = description
+		self.venue = venue
+
+	def __repr__(self):
+		return '<Offer {}: {}>'.format(self.id, self.title)
+
+	def toJSON(self):
+		whitelist = ['id', 'title', 'venue', 'description']
+		return {attr: getattr(self, attr) for attr in whitelist}
